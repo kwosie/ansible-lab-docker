a05b945e7ebc5e0b5fec575bd935436e01bd0148
Sarel Janse van Rensburg <sarel.jvrensburg@is.co.za>
2018-05-23 23:34:24 +0200

    Initial commit with centos

--- ansible/host/run.sh
+++ ansible/host/run.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 # add a master public key to authorized_keys on host in order to allow SSH connections
-cat /var/ans/master_key.pub >> /root/.ssh/authorized_keys
+whoami
+cat /usr/share/ansible/master_key >> /etc/ssh/ssh_host_rsa_key
+ls -la /usr/share/ansible
+ssh -i /etc/ssh/ssh_host_rsa_key master01
+#cat /etc/ssh/ssh_host_rsa_key
+ls -la /etc/ssh/ssh_host_rsa_key
+chmod 600 /etc/ssh/ssh_host_rsa_key
+mkdir -p /root/.ssh
+cat /usr/share/ansible/master_key.pub >> /root/.ssh/authorized_keys
 
 # start SSH server
+#bash
 /usr/sbin/sshd -D
